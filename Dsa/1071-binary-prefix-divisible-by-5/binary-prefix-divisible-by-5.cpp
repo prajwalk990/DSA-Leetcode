67beadb29caff67e5fca8cccf5fd4b8da943bbf3
Prajwal Kumbhar <kumbharprajwal455@gmail.com>
2026-09-25 01:08:32 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (95.87%) - LeetSync

---
+++ Dsa/1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<bool> prefixesDivBy5(vector<int>& nums) {
+        vector<bool> ans;
+        int rem = 0;
+
+        for(int x : nums) {
+            rem = (rem * 2 + x) % 5;
+            ans.push_back(rem == 0);
+        }
+
+        return ans;
+    }
+};
